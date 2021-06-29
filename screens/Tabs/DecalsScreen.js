f7fedc8abca23ee6c2aeed005b6a6b9b90805bd7
@johsol <johsol@jpj.com>
2021-06-30 06:57:34 +0800

    FEAT: Sending decals

--- screens/Tabs/DecalsScreen.js
+++ screens/Tabs/DecalsScreen.js
@@ -107,15 +107,51 @@ class Login extends Component {
     );
   }
 
+  _getdate() {
+      var d = new Date(),
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = d.getFullYear();
+  
+      if (month.length < 2) 
+          month = '0' + month;
+      if (day.length < 2) 
+          day = '0' + day;
+  
+      return [year, month, day].join('-');
+  }
+
   _submitPost() {
-      
+    axios({
+      url: 'https://serene-cliffs-80945.herokuapp.com/api',
+      method: 'POST',
+      data: {
+          query: `
+            mutation{
+              SendDecals(newDecal: {
+                Pic1: "${this.state.image}",
+                Pic2: "${this.state.imageBack}",
+                Date: "${this._getdate()}",
+                UserId: "${this.state.userId}"
+              }){
+                _id
+              }
+            }
+          `
+      }
+    }).then(result => {
+        alert('Decals Sent')
+    }).catch(err => {
+        alert(err)
+    })
   }
 
   _openCamera = async () => {
       let result = await ImagePicker.launchCameraAsync({
           allowsEditing: true,
           aspect: [4,3],
-          base64: true
+          base64: true,
+          quality: 0
       })
 
       if (!result.cancelled) {
@@ -127,7 +163,8 @@ class Login extends Component {
     let result = await ImagePicker.launchCameraAsync({
         allowsEditing: true,
         aspect: [4,3],
-        base64: true
+        base64: true,
+        quality: 0
     })
 
     if (!result.cancelled) {
