6ea91cb3443d5c2cb0e43b036c7886bc6a538ccf
@johsol <johsol@jpj.com>
2021-07-10 10:27:00 +0800

    FIX: Updating dashboard scenarios

--- screens/Tabs/DashboardScreen.js
+++ screens/Tabs/DashboardScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, ScrollView, FlatList, ActivityIndicator } from 'react-native';
+import { StyleSheet, ScrollView, FlatList, ActivityIndicator, Linking, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
@@ -16,6 +16,8 @@ export default class TabOneScreen extends Component {
   constructor(props) {
     super(props);
 
+    this._isMounted = false;
+
     this.state = {
       userId: null,
       allRequest: [],
@@ -24,13 +26,65 @@ export default class TabOneScreen extends Component {
         longitude: 'unknown', //Initial Longitude
         latitude: 'unknown', //Initial Latitude
       },
-      loading: true
+      loading: true,
+      activeBooking: false,
+      activeBookingDetails: null,
+      submitBtn: false
     }
   }
 
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
   componentDidMount = async () => {
+    this._isMounted = true;
+    
     await AsyncStorage.getItem('userId').then(result => {
       result ? this.setState({ userId: result }) : this.props.navigation.navigate('Login');
+    })
+
+    await axios({
+      url: 'https://serene-cliffs-80945.herokuapp.com/api',
+      method: 'POST',
+      data: {
+          query: `
+            {
+              DriverTrip(userId:"${this.state.userId}"){
+                _id
+                PickAddress
+                DropAddress
+                DropLat
+                DropLong
+                PickLat
+                PickLong
+                HubLocated
+                Notes
+                UserId {
+                  FName
+                  LName
+                  Username
+                  Email
+                  Phone
+                }
+              }
+            }
+          `
+      }
+    }).then(result => {
+      result.data.data.DriverTrip == null ?
+        this.setState({
+          activeBooking: false
+        })
+      :
+        this.setState({
+          activeBooking: true,
+          activeBookingDetails: result.data.data.DriverTrip
+        })
+    }).catch( err => {
+      this.setState({
+        activeBooking: false
+      })
     })
 
     await navigator.geolocation.getCurrentPosition(
@@ -55,12 +109,13 @@ export default class TabOneScreen extends Component {
           data: {
               query: `
                 {
-                  AllTransactions{
+                  RequestTransactions{
                     _id
                     UserId{
                       FName
                       LName
                       Email
+                      Phone
                     }
                     PickAddress
                     DropAddress
@@ -79,7 +134,12 @@ export default class TabOneScreen extends Component {
               `
           }
         }).then(result => {
-            result.data.data.AllTransactions.reverse().map(res => {
+          if (result.data.data.RequestTransactions.length == 0) {
+            this.setState({
+              loading: false
+            })
+          } else {
+            result.data.data.RequestTransactions.reverse().map(res => {
               
               var maxDistanceInKM = 10; // 500m distance
               // startPoint - center of perimeter
@@ -109,6 +169,9 @@ export default class TabOneScreen extends Component {
                 })
               : null
             })
+          }
+
+          // !this.state.activeBooking ? this._reload() : null
         }).catch(err => {
             alert(err)
         })
@@ -122,16 +185,16 @@ export default class TabOneScreen extends Component {
   _reload = () => {
     this.setState({
       allRequest: [],
-      loading: true
+      loading: false
     });
-
+    
     axios({
       url: 'https://serene-cliffs-80945.herokuapp.com/api',
       method: 'POST',
       data: {
           query: `
             {
-              AllTransactions{
+              RequestTransactions{
                 _id
                 UserId{
                   FName
@@ -155,7 +218,13 @@ export default class TabOneScreen extends Component {
           `
       }
     }).then(result => { 
-        result.data.data.AllTransactions.reverse().map(res => {
+
+      if (result.data.data.RequestTransactions.length == 0) {
+        this.setState({
+          loading: false
+        })
+      } else {
+        result.data.data.RequestTransactions.reverse().map(res => {
                 
           var maxDistanceInKM = 10; // 500m distance
           // startPoint - center of perimeter
@@ -184,12 +253,62 @@ export default class TabOneScreen extends Component {
             })
           : null
         })
+      }
     }).catch(err => {
         alert(err)
     })
   }
 
+  _activateTrip = () => {
+    axios({
+      url: 'https://serene-cliffs-80945.herokuapp.com/api',
+      method: 'POST',
+      data: {
+          query: `
+            {
+              DriverTrip(userId:"${this.state.userId}"){
+                _id
+                PickAddress
+                DropAddress
+                DropLat
+                DropLong
+                PickLat
+                PickLong
+                HubLocated
+                Notes
+                UserId {
+                  FName
+                  LName
+                  Username
+                  Email
+                  Phone
+                }
+              }
+            }
+          `
+      }
+    }).then(result => {
+      result.data.data.DriverTrip == null ?
+        this.setState({
+          activeBooking: false
+        })
+      :
+        this.setState({
+          activeBooking: true,
+          activeBookingDetails: result.data.data.DriverTrip
+        })
+    }).catch( err => {
+      this.setState({
+        activeBooking: false
+      })
+    })
+  }
+
   _accept = (id) => {
+    this.setState({
+      submitBtn: true
+    })
+    
     axios({
       url: 'https://serene-cliffs-80945.herokuapp.com/api',
       method: 'POST',
@@ -203,7 +322,10 @@ export default class TabOneScreen extends Component {
           `
       }
     }).then(result => {
-        this._reload()
+        this._activateTrip()
+        this.setState({
+          submitBtn: false
+        })
     }).catch(err => {
         alert(err)
     })
@@ -223,6 +345,9 @@ export default class TabOneScreen extends Component {
           `
       }
     }).then(result => {
+        this.setState({
+          activeBooking: false
+        })
         this._reload()
     }).catch(err => {
         alert(err)
@@ -231,71 +356,107 @@ export default class TabOneScreen extends Component {
 
   render () {
 
+    const {
+      activeBookingDetails
+    } = this.state
+
     return (
       <View style={styles.container}>
         {
-            this.state.loading ? 
-              <Card><ActivityIndicator></ActivityIndicator></Card> 
-            :
-              <View>
-                <Button
-                  title='refresh'
-                  onPress={() => this._reload()}
-                />
-                <FlatList
-                  style={{ paddingBottom: 10 }}
-                  data={this.state.allRequest}
-                  renderItem={({item}) => 
-                        <Card containerStyle={styles.Cards}>
-                          <Card.Title><Text h1>{ item.UserId.FName } { item.UserId.LName }</Text> {<Text>{item.Status}</Text>}</Card.Title>
-                            <Card.Divider/>
-                            {/* <Text style={{marginBottom: 10, color: 'black'}}>{item.HubLocated}</Text> */}
-                            {/* <Text style={{marginBottom: 10, color: 'black'}}>{JSON.stringify(item)}</Text> */}
-                            <Text h4 style={{ color: 'black'}}>Hub: {item.HubLocated}</Text>
-                            <Text h4 style={{ color: 'black'}}>Email: {item.UserId.Email}</Text>
-                            <Text h4 style={{ color: 'black'}}>Extra Notes: {item.Notes == "null" ? "No extra notes" : item.Notes}</Text>
+            !this.state.loading ? 
+              this.state.activeBooking ?
+                <View> 
+                  <Card>
+                    <Card.Title><Text h1>Active Request</Text></Card.Title>
+                    <Text h4 style={{ color: 'black'}}>Name: {activeBookingDetails.UserId.FName} {activeBookingDetails.UserId.LName}</Text>
+                    <Text h4 style={{ color: 'black'}}>Phone: <TouchableOpacity  onPress={() => Linking.openURL(`tel:${activeBookingDetails.UserId.Phone}`)}><Text>{activeBookingDetails.UserId.Phone}</Text></TouchableOpacity></Text>
+                    <Text style={{marginBottom: 10, color: 'black'}}>Pickup address: {activeBookingDetails.PickAddress}</Text>
+                    <NavigationApps
+                        iconSize={50}
+                        row
+                        address={activeBookingDetails.PickAddress} // address to navigate by for all apps 
+                        googleMaps={{activeBookingDetails: activeBookingDetails.PickLat, lon: activeBookingDetails.PickLong, action: actions.navigateByAddress, travelMode: 'driving'}}
+                        waze={{address: activeBookingDetails.PickAddress, lat: activeBookingDetails.PickLat, lon: activeBookingDetails.PickLong, action: actions.navigateByLatAndLon, travelMode: 'driving'}} // specific settings for waze
+                    />
+                    <Text style={{marginBottom: 10, color: 'black'}}>Drop Off address: {activeBookingDetails.DropAddress}</Text>
+                    <NavigationApps
+                        iconSize={50}
+                        row
+                        address={activeBookingDetails.DropAddress} // address to navigate by for all apps 
+                        googleMaps={{address: activeBookingDetails.DropAddress, lat: activeBookingDetails.DropLat, lon: activeBookingDetails.DropLong, action: actions.navigateByAddress, travelMode: 'driving'}}
+                        waze={{address: activeBookingDetails.DropAddress, lat: activeBookingDetails.DropLat, lon: activeBookingDetails.DropLong, action: actions.navigateByLatAndLon, travelMode: 'driving'}} // specific settings for waze
+                    />
 
-                            {item.Status == 'Accepted' ?
-                                <Card>
-                                  <Text style={{marginBottom: 10, color: 'black'}}>{item.PickAddress}</Text>
-                                  <NavigationApps
-                                      iconSize={50}
-                                      row
-                                      address={item.PickAddress} // address to navigate by for all apps 
-                                      googleMaps={{lat: item.PickLat, lon: item.PickLong, action: actions.navigateByAddress, travelMode: 'driving'}}
-                                      waze={{address: item.PickAddress, lat: item.PickLat, lon: item.PickLong, action: actions.navigateByLatAndLon, travelMode: 'driving'}} // specific settings for waze
-                                  />
-                                  <Text style={{marginBottom: 10, color: 'black'}}>{item.DropAddress}</Text>
-                                  <NavigationApps
-                                      iconSize={50}
-                                      row
-                                      address={item.DropAddress} // address to navigate by for all apps 
-                                      googleMaps={{address: item.DropAddress, lat: item.DropLat, lon: item.DropLong, action: actions.navigateByAddress, travelMode: 'driving'}}
-                                      waze={{address: item.DropAddress, lat: item.DropLat, lon: item.DropLong, action: actions.navigateByLatAndLon, travelMode: 'driving'}} // specific settings for waze
-                                  />
-                                </Card>
-                              :
-                                null
-                            }
+                    <Button
+                      disabled={this.state.submitBtn}
+                      title='Finish Trip'
+                      onPress={() => this._finishTrip(activeBookingDetails._id)}
+                    />
+                  </Card>
+                </View>
+              :
+                <View>
+                  <Button
+                    title='refresh'
+                    onPress={() => this._reload()}
+                  />
+                  <FlatList
+                    style={{ paddingBottom: 10 }}
+                    data={this.state.allRequest}
+                    renderItem={({item}) => 
+                          <Card containerStyle={styles.Cards}>
+                            <Card.Title><Text h1>{ item.UserId.FName } { item.UserId.LName }</Text> {<Text>{item.Status}</Text>}</Card.Title>
+                              <Card.Divider/>
+                              {/* <Text style={{marginBottom: 10, color: 'black'}}>{item.HubLocated}</Text> */}
+                              {/* <Text style={{marginBottom: 10, color: 'black'}}>{JSON.stringify(item)}</Text> */}
+                              <Text h4 style={{ color: 'black'}}>Hub: {item.HubLocated}</Text>
+                              <Text h4 style={{ color: 'black'}}>Email: {item.UserId.Email}</Text>
+                              <Text h4 style={{ color: 'black'}}>Extra Notes: {item.Notes == "null" ? "No extra notes" : item.Notes}</Text>
 
-                            {
-                              item.Status == 'Pending' ?
-                                <Button
-                                  title='Accept'
-                                  onPress={() => this._accept(item._id)}
-                                />
-                                :
-                                  item.Status == 'Accepted' ?
-                                    <Button
-                                      title='Finish Trip'
-                                      onPress={() => this._finishTrip(item._id)}
+                              {item.Status == 'Accepted' ?
+                                  <Card>
+                                    <Text style={{marginBottom: 10, color: 'black'}}>{item.PickAddress}</Text>
+                                    <NavigationApps
+                                        iconSize={50}
+                                        row
+                                        address={item.PickAddress} // address to navigate by for all apps 
+                                        googleMaps={{lat: item.PickLat, lon: item.PickLong, action: actions.navigateByAddress, travelMode: 'driving'}}
+                                        waze={{address: item.PickAddress, lat: item.PickLat, lon: item.PickLong, action: actions.navigateByLatAndLon, travelMode: 'driving'}} // specific settings for waze
                                     />
-                                  : null
-                            }
-                        </Card>
-                  }
-                />
-              </View>
+                                    <Text style={{marginBottom: 10, color: 'black'}}>{item.DropAddress}</Text>
+                                    <NavigationApps
+                                        iconSize={50}
+                                        row
+                                        address={item.DropAddress} // address to navigate by for all apps 
+                                        googleMaps={{address: item.DropAddress, lat: item.DropLat, lon: item.DropLong, action: actions.navigateByAddress, travelMode: 'driving'}}
+                                        waze={{address: item.DropAddress, lat: item.DropLat, lon: item.DropLong, action: actions.navigateByLatAndLon, travelMode: 'driving'}} // specific settings for waze
+                                    />
+                                  </Card>
+                                :
+                                  null
+                              }
+
+                              {
+                                item.Status == 'Pending' ?
+                                  <Button
+                                    disabled={this.state.submitBtn}
+                                    title='Accept'
+                                    onPress={() => this._accept(item._id)}
+                                  />
+                                  :
+                                    item.Status == 'Accepted' ?
+                                      <Button
+                                        title='Finish Trip'
+                                        onPress={() => this._finishTrip(item._id)}
+                                      />
+                                    : null
+                              }
+                          </Card>
+                    }
+                  />
+                </View>
+            :
+              <Card><ActivityIndicator></ActivityIndicator></Card>
         }
       </View>
     );
